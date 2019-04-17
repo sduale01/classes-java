95516b8c0c6817422b941be66744095b060a7ab3
Sharmarke Duale <sduale01@gmail.com>
2019-04-17 16:17:49 -0500

    calculate multiples of 3 and 5 using java

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,14 @@
+import java.io.*;
+
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Multiples of 3 and 5
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i + " is divisible by 3");
+            } else if (i % 5 == 0) {
+                System.out.println(i + " is divisible by 5");
+            }
+        }
     }
 }
